7443b02add59a73b6396bf44665c4ff32651f20d
pedro2806 <isc.pedro.mtz@gmail.com>
2025-04-09 09:17:54 -0600

    Se modifica login
    Se modifica login

--- index.php
+++ index.php
@@ -7,32 +7,12 @@
     <meta name = "description" content = "">
     <meta name = "author" content = "">
 
-    <title>MESS - Modulo de solicitud de vacaciones</title>
+    <title>MESS - Control Vehicular</title>
 
     <!-- Custom fonts for this template-->
-    <link href = "vendor/fontawesome-free/css/all.min.css" rel = "stylesheet" type = "text/css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.css">
+    <link href = "vendor/fontawesome-free/css/all.min.css" rel = "stylesheet" type = "text/css">    
     <!-- Custom styles for this template-->
     <link href = "css/sb-admin-2.min.css" rel = "stylesheet">
-
-    <style>
-        .bg-login-image {
-            background-image: url('img/MESS_05_Imagotipo.svg');
-            background-size: contain;
-            background-position: center;
-            background-repeat: no-repeat; 
-            width: 100%; /* Ocupar el ancho completo del contenedor */
-            max-width: 300px; /* Ancho máximo */
-            height: 200px; /* Altura fija */
-            margin: 0 auto; /* Centrar la imagen */
-        }
-        @media (max-width: 767.98px) { /* Para pantallas pequeñas */
-            .bg-login-image {
-                max-width: 200px; /* Reducir el ancho máximo */
-                height: 150px; /* Reducir la altura */
-            }
-        }
-    </style>
 </head>
 <body class = "bg-gradient-primary">
     <div class = "container">
@@ -40,20 +20,20 @@
             <div class = "col-xl-10 col-lg-12 col-md-9">
                 <div class = "card o-hidden border-0 shadow-lg my-5">
                     <div class = "card-body p-0">
-                        <div class = "row">
-                            <div class="d-lg-block bg-login-image"></div>
+                        <div class = "row justify-content-center">                                                        
+                            <div class = "p-0 text-center">                                    
+                                <img src = "img/MESS_05_Imagotipo.svg" alt = "Logo MESS" width = "300px">
+                            </div>                            
                         </div>      
                         <div class = "row">
                             <!--LOGIN-->
-                            <div class = "col-lg-6">
-                                <div class = "p-0">
+                            <div class = "col-sm-2"></div>
+                            <div class = "col-sm-8 d-flex flex-column align-items-center">
+                                <div class = "p-0 w-100">
                                     <div class = "text-center">
-                                    <center>
                                         <b>
-                                            RR HH<br>
-                                            Incidencias
+                                            Control Vehicular
                                         </b>
-                                    </center>
                                         <h1 class = "h4 text-gray-900 mb-4">Bienvenido</h1>
                                     </div>
                                     <form class = "user" method = "POST">
@@ -70,28 +50,22 @@
                                                 <label class = "custom-control-label" for = "customCheck">Recordar usuario y contraseña</label>
                                             </div>
                                         </div>
-                                        <center>
-                                            <input class = "btn btn-primary" type = "submit" name = "btningresar" value = "   Acceder   "/>                                       
-                                        </center>
-                                        <!--<a class = "small" href = "forgot-password">Olvide mi contraseña</a>--> 
+                                        <div class="text-center">
+                                            <input class = "btn btn-primary" type = "submit" name = "btningresar" value = "   Acceder   "/>
+                                        </div>
+                                        <!--<a class = "small" href = "forgot-password">Olvide mi contraseña</a>-->
                                         <br>
                                         <br>
                                         <br>
-                                    </form>                      
+                                    </form>
                                 </div>
                             </div>
-                            <!--AGENDA SALA DE JUNTAS-->
-                            <div class = "col-lg-6">
-                                <div class = "p-0">
-                                    <div class = "text-center" id='calendar'></div>                             
-                                </div>
-                            </div>  
                         </div>
                         <!--BARRA DE SOPORTE-->
                         <div class="row">  
                             <div class = "col-lg-12 mx-auto">
                                 <center>
-                                    <p class="alert alert-info">
+                                    <p class="alert alert-info" style="font-size: 0.9em;">
                                         Soporte del sistema:
                                         <a href="mailto:sebastian.gutierrez@mess.com.mx">sebastian.gutierrez@mess.com.mx</a> 
                                         o 
@@ -121,17 +95,22 @@
             $usuario = explode('@', $usuario)[0];
             
             $Qempresas  =  "SELECT  *, TIMESTAMPDIFF(YEAR,fechaIngreso,CURDATE()) AS antiguedad, rol FROM usuarios WHERE usuario  = '".$usuario."@mess.com.mx' and password  =  '".$pass."' AND estatus = 1";
-            $res2 =  mysqli_query( $conn, $Qempresas ) or die (mysqli_error($conn));
+            $res2 = mysqli_query($conn, $Qempresas);
+            
+            if (!$res2) {
+                die("Error in query execution: " . mysqli_error($conn));
+            }
+            
             $nr = mysqli_num_rows($res2);
             
-            While ($row2 = mysqli_fetch_array($res2)){
-                $nombreEmpleado = $row2["nombre"];
-                $noEmpleado = $row2["noEmpleado"];
-                $antiguedad = $row2["antiguedad"];
-                $diasD = $row2["diasdisponibles"];
-                $rol = $row2["rol"];
-                
-                
+            if ($nr > 0) {
+                while ($row2 = mysqli_fetch_array($res2)) {
+                    $nombreEmpleado = $row2["nombre"];
+                    $noEmpleado = $row2["noEmpleado"];
+                    $antiguedad = $row2["antiguedad"];
+                    $diasD = $row2["diasdisponibles"];
+                    $rol = $row2["rol"];
+                }
             }
     
             if($nr == 1)
@@ -160,35 +139,12 @@
     ?>
     <script src = "https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src = "vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src = "js/sb-admin-2.min.js"></script>
-    <script src = "https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.js"></script>
-    <script src = "vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src = "js/sb-admin-2.min.js"></script>    
+
     <script>
     $(document).ready(function () {
-        verCalendarioLogin();
-    });
-
-        function verCalendarioLogin() {
-            var calendarEl = document.getElementById('calendar');
-
-            var calendar = new FullCalendar.Calendar(calendarEl, {        
-                initialView: 'listWeek', // Cambiar a vista diaria
-                events: 'SalaDeJuntas/acciones_calendarioGral.php?opcion=login', // Aquí llamas a tu PHP que devuelve las vacaciones en JSON
-                editable: false,
-                locale: 'es',
-                eventContent: function(info) {
-                    // Personalizar el contenido del evento
-                    var nombreEmpleado = info.event.title;
-                    var fechaInicio = info.event.start;
-                    var fechaFin = info.event.end;
-                    var descripcion = info.event.extendedProps.descripcion || 'Sin descripción'; // Obtener la descripción del evento
-                    var displayText = nombreEmpleado + '<br>' + descripcion;
-
-                    return { html: displayText };
-                }
-            });
-            calendar.render();
-        } 
+        
+    });        
     </script>
 </body>
 </html>

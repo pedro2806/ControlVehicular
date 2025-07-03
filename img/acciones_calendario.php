5b782e03ce6b115e181acfd9c963daae5b9903af
pedro2806 <isc.pedro.mtz@gmail.com>
2025-07-03 10:03:14 -0600

    Actualizacion calendario planeadas
    Actualizacion calendario planeadas

--- img/acciones_calendario.php
+++ img/acciones_calendario.php
@@ -4,6 +4,9 @@ header('Content-Type: application/json');
 mysqli_set_charset($conn, "utf8mb4");
 date_default_timezone_set('America/Mexico_City');
 $accion = isset($_POST['accion']) ? $_POST['accion'] : '';
+$area = isset($_POST['area']) ? $_POST['area'] : '';
+$ingeniero = isset($_POST['ing']) ? $_POST['ing'] : '';
+
 
 if ($accion == 'CargarVehiculos'){
     $sql = "SELECT id_vehiculo, placa, marca, modelo, color, '' as id_prestamo, '' as estatus
@@ -33,19 +36,72 @@ if ($accion == 'CargarVehiculos'){
 if($accion == 'ActividadesCalendarioPlaneadas'){
     // Consultar las actividades planeadas del usuario actual
     $fechaHoy = date('Y-m-d');
-    $fechaInicio = date('Y-m-d', strtotime($fechaHoy . ' -2 days'));
+    $fechaInicio = date('Y-m-d', strtotime($fechaHoy . ' -10 days'));
 
-    $sql = "SELECT ot.*, DATE(ot.FechaPlaneadaInicio) as FechaPlaneadaInicioDate, c.nombre as cliente
+    /*$sql = "SELECT ot.*, DATE(ot.FechaPlaneadaInicio) as FechaPlaneadaInicioDate, c.nombre as cliente
             FROM ordenes_servicio ot
             LEFT JOIN clientes c ON ot.customer_id = c.id_cliente
-            WHERE DATE(ot.FechaPlaneadaInicio) >= '$fechaInicio' AND ot.qualityAreas NOT IN ('qualityAreas') AND ot.status IN ('Asignada', 'Trabajando') AND ot.engineers != ''";
+            WHERE DATE(ot.FechaPlaneadaInicio) >= '$fechaInicio' AND ot.qualityAreas NOT IN ('qualityAreas') AND ot.status IN ('Asignada', 'Trabajando') AND ot.engineers != ''";*/
+    $sql = "SELECT ot.*, DATE(ot.start_date) as FechaPlaneadaInicioDate, c.nombre as cliente
+            FROM servicios_planeados ot
+            LEFT JOIN clientes c ON ot.id_cliente = c.id_cliente
+            WHERE DATE(ot.start_date) >= '$fechaInicio'";
     $result = $conn->query($sql);
+    
     if ($result && $result->num_rows > 0) {
         $actividades = [];
         while ($row = $result->fetch_assoc()) {
             $actividades[] = $row;
         }
         echo json_encode(['status' => 'success', 'actividades' => $actividades]);
-    }   
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'No se encontraron actividades planeadas o error en la consulta.']);
+    }
+}
+
+
+
+if($accion == 'ActividadesCalendarioPlaneadasfiltro'){
+    // Consultar las actividades planeadas del usuario actual
+    $fechaHoy = date('Y-m-d');
+    $fechaInicio = date('Y-m-d', strtotime($fechaHoy . ' -10 days'));
+
+    /*$sql = "SELECT ot.*, DATE(ot.FechaPlaneadaInicio) as FechaPlaneadaInicioDate, c.nombre as cliente
+            FROM ordenes_servicio ot
+            LEFT JOIN clientes c ON ot.customer_id = c.id_cliente
+            WHERE DATE(ot.FechaPlaneadaInicio) >= '$fechaInicio' AND ot.qualityAreas NOT IN ('qualityAreas') AND ot.status IN ('Asignada', 'Trabajando') AND ot.engineers != ''";*/
+    $sql = "SELECT ot.*, DATE(ot.start_date) as FechaPlaneadaInicioDate, c.nombre as cliente
+            FROM servicios_planeados ot
+            LEFT JOIN clientes c ON ot.id_cliente = c.id_cliente
+            WHERE DATE(ot.start_date) >= '$fechaInicio'";
+    
+
+    $whereClauses = [];
+
+    if (!empty($area)) {
+        $area = $conn->real_escape_string($area);
+        $whereClauses[] = "REPLACE(SUBSTRING_INDEX(ot.order_code, '-', 1), '25', '') =  '$area'";
+    }
+
+    if (!empty($ingeniero)) {
+        $ingeniero = $conn->real_escape_string($ingeniero);
+        $whereClauses[] = "ot.engineer LIKE '%" . $ingeniero . "%'";
+    }
+
+    if (!empty($whereClauses)) {
+        $sql .= " AND " . implode(' AND ', $whereClauses);
+    }
+    //echo $sql;
+    $result = $conn->query($sql);
+    
+    if ($result && $result->num_rows > 0) {
+        $actividades = [];
+        while ($row = $result->fetch_assoc()) {
+            $actividades[] = $row;
+        }
+        echo json_encode(['status' => 'success', 'actividades' => $actividades]);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'No se encontraron actividades planeadas o error en la consulta.']);
+    }
 }
 ?>

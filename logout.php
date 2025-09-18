e39c0ebde5f941b352883924fcdddc20e7613226
pedro2806 <isc.pedro.mtz@gmail.com>
2025-09-18 10:35:16 -0600

    Login Master
    Se suben archivos de validacion para el login master

--- logout.php
+++ logout.php
@@ -17,17 +17,31 @@
 	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
 	<script type="text/javascript" class="init">
 	
-$(document).ready(function() {
-    //document.cookie = "antiguedad='.$antiguedad.';expires=" + new Date(Date.now() + 9600000).toUTCString() + ";SameSite=Lax;";
-    document.cookie = "antiguedad =00; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/ControlVehicular;";
-    document.cookie = "diasD =00; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/ControlVehicular;";
-    document.cookie = "noEmpleado =00; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/ControlVehicular;";
-    document.cookie = "nombredelusuario =00; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/ControlVehicular;";
-    document.cookie = "rol =00; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/ControlVehicular;";
+		$(document).ready(function() {
+			//document.cookie = "antiguedad='.$antiguedad.';expires=" + new Date(Date.now() + 9600000).toUTCString() + ";SameSite=Lax;";
+			document.cookie = "antiguedad =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "diasD =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "noEmpleado =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "nombredelusuario =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "rol =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 
-    window.location.assign("../loginMaster/inicio")        
-} );
+			let cookieSesion = getCookie("SesionLogin");
+					//alert("Cerrando sesi贸n..." + cookieSesion);
+			if (cookieSesion === "LoginMaster") {
+				document.cookie = "SesionLogin=00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+				window.location.assign("../loginMaster/inicio");
+			} else {
+				window.location.assign("https://www.messbook.com.mx/ControlVehicular/");
+			}
+		});
 
+		//Funcion para leer cookies
+		function getCookie(name) {
+			let value = "; " + document.cookie;
+			let parts = value.split("; " + name + "=");
+			if (parts.length === 2) return parts.pop().split(";").shift();
+			return null; // Si no encuentra la cookie, retorna null
+		}
 	</script>
 
 </head>
--- validaLoginMaster.php
+++ validaLoginMaster.php
@@ -4,21 +4,17 @@ include 'conn.php';
 $accion = isset($_POST['accion']) ? $_POST['accion'] : '';
 
 // Validación de usuario (otro caso)
-$id_usuario = isset($_POST['id_usuario']) ? $_POST['id_usuario'] : '';
-$nombredelusuario = isset($_POST['nombredelusuario']) ? $_POST['nombredelusuario'] : '';
-$noEmpleado = isset($_POST['noEmpleado']) ? $_POST['noEmpleado'] : '';
-$rol = isset($_POST['rol']) ? $_POST['rol'] : '';
-$usuario = isset($_POST['correo']) ? $_POST['correo'] : '';
+$id_usuario = isset($_POST['id_usuarioCV']) ? $_POST['id_usuarioCV'] : '';
+$nombredelusuario = isset($_POST['nombredelusuarioCV']) ? $_POST['nombredelusuarioCV'] : '';
+$noEmpleado = isset($_POST['noEmpleadoCV']) ? $_POST['noEmpleadoCV'] : '';
+$rol = isset($_POST['rolCV']) ? $_POST['rolCV'] : '';
+$usuario = isset($_POST['correoCV']) ? $_POST['correoCV'] : '';
 
-if (empty($id_usuario) || empty($nombredelusuario) || empty($noEmpleado) || empty($rol)) {
-    http_response_code(400);
-    echo json_encode(['success' => false, 'message' => 'Datos incompletos.']);
-    exit;
-} else {
+
     $Qempresas  =  "SELECT  *, TIMESTAMPDIFF(YEAR,fechaIngreso,CURDATE()) AS antiguedad, rol FROM usuarios WHERE usuario  = '".$usuario."' AND estatus = 1";
     $res2 =  mysqli_query( $conn, $Qempresas ) or die (mysqli_error($conn));
     $nr = mysqli_num_rows($res2);
-
+echo $Qempresas;
     while ($row2 = mysqli_fetch_array($res2)){
         $id_usuario = $row2["id"];
         $nombreEmpleado = $row2["nombre"];
@@ -29,24 +25,12 @@ if (empty($id_usuario) || empty($nombredelusuario) || empty($noEmpleado) || empt
     }
 
     if($nr == 1){
-        if (!isset($_COOKIE['antiguedad']) || $_COOKIE['antiguedad'] != $antiguedad) {
-            setcookie('antiguedad', $antiguedad, time() + 604800, "/ControlVehicular", "", false, true);
-        }
-        if (!isset($_COOKIE['nombredelusuario']) || $_COOKIE['nombredelusuario'] != $nombreEmpleado) {
-            setcookie('nombredelusuario', $nombreEmpleado, time() + 604800, "/ControlVehicular", "", false, true);
-        }
-        if (!isset($_COOKIE['noEmpleado']) || $_COOKIE['noEmpleado'] != $noEmpleado) {
-            setcookie('noEmpleado', $noEmpleado, time() + 604800, "/ControlVehicular", "", false, true);
-        }
-        if (!isset($_COOKIE['diasD']) || $_COOKIE['diasD'] != $diasD) {
-            setcookie('diasD', $diasD, time() + 604800, "/ControlVehicular", "", false, true);
-        }
-        if (!isset($_COOKIE['rol']) || $_COOKIE['rol'] != $rol) {
-            setcookie('rol', $rol, time() + 604800, "/ControlVehicular", "", false, true);
-        }
-        if (!isset($_COOKIE['id_usuario']) || $_COOKIE['id_usuario'] != $id_usuario) {
-            setcookie('id_usuario', $id_usuario, time() + 604800, "/ControlVehicular", "", false, true);
-        }
+        echo '<script>document.cookie = "id_usuario='.$id_usuario.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "nombredelusuario='.$nombreEmpleado.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "noEmpleado='.$noEmpleado.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "rol='.$rol.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "SesionLogin=LoginMaster; expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>window.location.assign("inicio")</script>';
 
         session_start();
         $_SESSION['nombredelusuario'] = $nombreEmpleado;
@@ -58,14 +42,12 @@ if (empty($id_usuario) || empty($nombredelusuario) || empty($noEmpleado) || empt
         echo json_encode(['success' => true]);
         exit;
     }
-    //if (!isset($_COOKIE['antiguedad']) || $_COOKIE['antiguedad'] != $antiguedad) {
-       //     setcookie('Lantiguedad', $antiguedad, time() + 604800, "/", "", false, true);
-        //}
+
     // Si no hay usuario válido
     echo json_encode([
         'success' => false,
         'message' => 'Usuario no válido.',
     ]);
     exit;
-}
+
 ?>

9b7c8b26740484607bd90e0c4ae471b78e174db9
pedro2806 <isc.pedro.mtz@gmail.com>
2026-01-20 11:05:57 -0600

    Update acciones_kilometraje.php

--- acciones_kilometraje.php
+++ acciones_kilometraje.php
@@ -97,7 +97,7 @@ if ($accion == 'CargarVehiculos'){
 if ($accion == 'CargarVehiculosPTenencia'){
     $sql = "SELECT inv.id_vehiculo, inv.placa, inv.marca, inv.modelo
             FROM inventario inv
-            INNER JOIN prestamos p ON inv.id_vehiculo = p.id_vehiculo
+            INNER JOIN prestamos p ON inv.id_vehiculo = p.id_vehiculo AND p.estatus = 'AUTORIZADO'
             WHERE p.id_usuario = '".$_COOKIE['id_usuario']."'
             UNION
             SELECT id_vehiculo, placa, marca, modelo
